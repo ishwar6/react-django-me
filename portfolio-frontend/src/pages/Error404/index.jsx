190f9ba2c19c466ca58c3395bbf084351321c8fd
ishwar6 <ishwarj116@gmail.com>
2023-08-09 02:58:40 +0000

    Added Button in Error page

--- portfolio-frontend/src/pages/Error404/index.jsx
+++ portfolio-frontend/src/pages/Error404/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Error404() {
+    const navigate = useNavigate();
     return (
         <div id="ftco-loader" className="show fullscreen" style={{ flexDirection: "column" }}>
             <div className="mars"></div>
             <img src="https://assets.codepen.io/1538474/404.svg" className="logo-404" />
             <img src="https://assets.codepen.io/1538474/meteor.svg" className="meteor" />
-            <h1 style={{ color: "black" }} className="title">Oops something went wrong!!</h1>
+            <h2 style={{ color: "black" }} className="title">Oops something went wrong!!</h2>
+            <a onClick={() =>{
+                navigate('/')
+            }} className="btn btn-primary my-3 py-2 px-4">
+                Go to Home
+            </a>
             <img src="https://assets.codepen.io/1538474/astronaut.svg" className="astronaut" />
             <img src="https://assets.codepen.io/1538474/spaceship.svg" className="spaceship" />
         </div>
